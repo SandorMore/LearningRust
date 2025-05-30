dd2dfaeea0e658d149b935bea8d8c4b04559773d
SandorMore <msaso0725@gmail.com>
2025-05-30 19:51:48 +0200

    while loop

--- Learning2.rs
+++ Learning2.rs
@@ -1,8 +1,12 @@
 fn main(){
     let a = 5;
     let b = 10;
+    let c = 0:
 
-
+    while c < 5{
+        println!("c is {}", c);
+        c += 1;
+    }
     let draw = |x, y| {
         println!("Drawing {} and {}", x, y);
     };
