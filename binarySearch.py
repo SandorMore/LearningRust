4553470a79e6dce9730d56f365d1ace3fe1353cd
SandorMore <msaso0725@gmail.com>
2025-06-08 11:34:07 +0200

    tf is thisd

--- binarySearch.py
+++ binarySearch.py
@@ -1,17 +1,2 @@
-list: list = [1, 2, 3, 4, 6, 7]
-target: int = 10
-
-left, right = 0, len(list) - 1
-found = False
-
-while left <= right:
-    mid = (left + right) // 2
-    if list[mid] == target:
-        found = True
-        break
-    elif list[mid] < target:
-        left = mid + 1
-    else:
-        right = mid - 1
-
-print("Found!" if found else "Not found.")
+a = 1
+print(hex(id(a)))
